9c524fd53b772340678cb770442418396cdc5dd9
chuangyu <createinfinite@yahoo.com.tw>
2015-05-22 12:01:01 +0800

    Add new file: FluidFX.hpp/cpp

---
+++ Source/FluidFX.cpp
@@ -0,0 +1,7 @@
+#include "Stdafx.hpp"
+#include "FluidFX.hpp"
+
+/*  Fluid Simulation Implementation comes from
+ *  Philip Rideout
+ *  http://prideout.net/blog/?p=58
+ */
---
+++ Source/FluidFX.hpp
@@ -0,0 +1,13 @@
+#ifndef FLUIDFX_HPP
+#define FLUIDFX_HPP
+
+#include "TextureObject.hpp"
+
+/*  Fluid Simulation Implementation comes from
+ *  Philip Rideout
+ *  http://prideout.net/blog/?p=58
+ */
+
+
+
+#endif // FLUIDFX_HPP
